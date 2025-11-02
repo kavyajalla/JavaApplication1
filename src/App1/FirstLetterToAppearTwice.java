f87f0660ba4352ae90b98c058b449576af6c0434
kavyaj <kavyajalla139@gmail.com>
2025-11-02 12:42:31 -0500

    First Letter to appear twice

---
+++ src/App1/FirstLetterToAppearTwice.java
@@ -0,0 +1,76 @@
+package App1;
+
+public class FirstLetterToAppearTwice {
+	 public char repeatedCharacter(String s) {
+	        boolean[] seen = new boolean[26];
+	        for(char c : s.toCharArray()) 
+	        {
+	            if(seen[c - 'a'] == true)
+	            {
+	                return c;
+	            }
+	            seen[c-'a'] = true;
+	        }
+	        return ' ';
+	    }
+	 
+	 /*
+	  * Alternate
+	  public char repeatedCharacter(String s) {
+	        Set<Character> l = new HashSet<>();
+	        for(int i=0; i<s.length() ; i++)
+	        {
+	            char c = s.charAt(i);
+	            if(l.contains(c))
+	            {
+	                return c;
+	            }
+	            l.add(c);
+	        }
+	        return ' ';
+	    }
+	*/
+}
+
+
+/*
+ * 2351. First Letter to Appear Twice
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given a string s consisting of lowercase English letters, return the first letter to appear twice.
+
+Note:
+
+A letter a appears twice before another letter b if the second occurrence of a is before the second occurrence of b.
+s will contain at least one letter that appears twice.
+ 
+
+Example 1:
+
+Input: s = "abccbaacz"
+Output: "c"
+Explanation:
+The letter 'a' appears on the indexes 0, 5 and 6.
+The letter 'b' appears on the indexes 1 and 4.
+The letter 'c' appears on the indexes 2, 3 and 7.
+The letter 'z' appears on the index 8.
+The letter 'c' is the first letter to appear twice, because out of all the letters the index of its second occurrence is the smallest.
+Example 2:
+
+Input: s = "abcdd"
+Output: "d"
+Explanation:
+The only letter that appears twice is 'd' so we return 'd'.
+ 
+
+Constraints:
+
+2 <= s.length <= 100
+s consists of lowercase English letters.
+s has at least one repeated letter.
+ * 
+ */
